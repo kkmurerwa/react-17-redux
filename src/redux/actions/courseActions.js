517e2ace9e48e3c58f1abbde6912bd76ea5227bb
Kenneth Murerwa <xwaxes@gmail.com>
2023-09-09 18:10:27 +0300

    feat: Added ability to create and manage courses

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,17 +1,24 @@
 import * as Types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return {
-    type: Types.CREATE_COURSE,
-    course,
-  };
-}
-
 export function loadCoursesSuccess(courses) {
   return {
     type: Types.LOAD_COURSES_SUCCESS,
     courses,
+  };
+}
+
+export function createCourseSuccess(course) {
+  return {
+    type: Types.CREATE_COURSE_SUCCESS,
+    course,
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return {
+    type: Types.UPDATE_COURSE_SUCCESS,
+    course,
   };
 }
 
@@ -21,6 +28,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
